31b636e67e220f152b1a82751eb29a35307a6748
Alvydas Vitkauskas <alvydas@ondato.com>
2023-12-07 19:45:47 +0200

    day 07

--- src/day07.cr
+++ src/day07.cr
@@ -1,6 +1,6 @@
 hands = File.read_lines("input/input07.txt").map do |line|
   cards, bid = line.split
-  {cards: cards.chars, bid: bid.to_i}
+  {cards.chars, bid.to_i}
 end
 
 CARDS_NO_JOCKER   = "23456789TJQKA"
@@ -20,15 +20,15 @@ def hand_type_with_joker(cards)
 end
 
 ans = hands
-  .sort_by! { |h| {hand_type_no_jocker(h[:cards]), h[:cards].map { |c| CARDS_NO_JOCKER.index!(c) }} }
-  .map_with_index { |h, i| h[:bid] * (i + 1) }
+  .sort_by! { |(cards, _)| {hand_type_no_jocker(cards), cards.map { |c| CARDS_NO_JOCKER.index!(c) }} }
+  .map_with_index { |(_, bid), i| bid * (i + 1) }
   .sum
 
 puts "Part 1: #{ans}"
 
 ans = hands
-  .sort_by! { |h| {hand_type_with_joker(h[:cards]), h[:cards].map { |c| CARDS_WITH_JOCKER.index!(c) }} }
-  .map_with_index { |h, i| h[:bid] * (i + 1) }
+  .sort_by! { |(cards, _)| {hand_type_with_joker(cards), cards.map { |c| CARDS_WITH_JOCKER.index!(c) }} }
+  .map_with_index { |(_, bid), i| bid * (i + 1) }
   .sum
 
 puts "Part 2: #{ans}"
