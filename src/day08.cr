cf20acdad25253ad9ae3a1473974672470e9e872
Alvydas Vitkauskas <alvydas@ondato.com>
2023-12-08 14:37:24 +0200

    day 08

---
+++ src/day08.cr
@@ -0,0 +1,34 @@
+require "big"
+
+moves, maps = File.read("input/input08.txt").split("\n\n")
+
+moves = moves.chars.map { |c| c == 'L' ? 0 : 1 }
+
+maps = maps.lines.map &.scan(/[0-9A-Z]+/).map(&.[0])
+maps = Hash.zip(maps.map(&.[0]), maps.map { |(_, l, r)| [l, r] })
+
+ans = 0
+node = "AAA"
+moves.cycle do |move|
+  node = maps[node][move]
+  ans += 1
+  break if node == "ZZZ"
+end
+
+puts "Part 1: #{ans}"
+
+nodes = maps.keys.select { |k| k.ends_with?("A") }
+
+steps = nodes.map_with_index do |node, i|
+  n = 0
+  moves.cycle do |move|
+    node = maps[node][move]
+    n += 1
+    break if node.ends_with?("Z")
+  end
+  BigInt.new(n)
+end
+
+ans = steps.reduce { |res, n| res.lcm(n) }
+
+puts "Part 2: #{ans}"
